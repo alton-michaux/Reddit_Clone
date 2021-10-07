011ace7ec0e1754475a104a9dcbed517b5ffc329
Alton Michaux <alton.michaux@yahoo.com>
2021-10-06 23:10:58 -0400

    <edit>cant get edit templates/variables to cooperate

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,8 +3,8 @@
 class PublicController < ApplicationController
   def index
     @communities = Community.all.limit(5)
-    @posts = Post.all
+    @posts = Post.order(id: :desc).limit(20)
   end
 
-  
+
 end
