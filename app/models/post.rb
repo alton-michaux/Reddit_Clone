883f7e295c6b73b7b486e217d4e6eea152ede5cd
Alton Michaux <alton.michaux@yahoo.com>
2021-10-06 20:23:49 -0400

    <posts>got posts up and running, also displaying to front page

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Post < ApplicationRecord
+  belongs_to :account
+  belongs_to :community
+
+  validates_presence_of :title, :body, :account_id, :community_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,31 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_005_211_333) do
-  create_table 'accounts', force: :cascade do |t|
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'username'
-    t.text 'bio'
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_accounts_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_accounts_on_reset_password_token', unique: true
+ActiveRecord::Schema.define(version: 2021_10_06_201504) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.text "bio"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_accounts_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
 
-  create_table 'communities', force: :cascade do |t|
-    t.integer 'account_id'
-    t.string 'name'
-    t.string 'url'
-    t.integer 'total_members'
-    t.text 'rules'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['account_id'], name: 'index_communities_on_account_id'
+  create_table "communities", force: :cascade do |t|
+    t.integer "account_id"
+    t.string "name"
+    t.string "url"
+    t.integer "total_members"
+    t.text "rules"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_communities_on_account_id"
   end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "account_id"
+    t.integer "community_id"
+    t.string "title"
+    t.text "body"
+    t.integer "upvotes", default: 0
+    t.integer "downvotes", default: 0
+    t.integer "total_comments", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_posts_on_account_id"
+    t.index ["community_id"], name: "index_posts_on_community_id"
+  end
+
 end
