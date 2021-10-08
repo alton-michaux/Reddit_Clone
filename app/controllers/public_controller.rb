4ede9f83c94469310fa44cfdb87a16d9cf1ef1de
Alton Michaux <alton.michaux@yahoo.com>
2021-10-08 15:51:52 -0400

    <style>added link and styling elements to posts/_lists.html.erb

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -5,9 +5,4 @@ class PublicController < ApplicationController
     @communities = Community.all.limit(5)
     @posts = Post.order(id: :desc).limit(20)
   end
-<<<<<<< HEAD
-
-
-=======
->>>>>>> cd16a5dc3e01152f2d4e43b5f49e1bf704eb7536
 end
