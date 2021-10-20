14a7b34bacbaffd99310b331a67a68224eb73b88
Alton Michaux <alton.michaux@yahoo.com>
2021-10-20 08:43:20 -0400

    <controller>added private method to communities_controller to set the community for show/update/edit views

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -4,6 +4,7 @@ class CommunitiesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :catch_not_found
   rescue_from StandardError, with: :catch_no_method
   before_action :authenticate_account!, except: %i[index show]
+  before_action :set_community, only: %i[show edit update destroy]
 
   def index
     @communities = Community.all
@@ -26,6 +27,12 @@ class CommunitiesController < ApplicationController
       format.html { redirect_to communities_url, notice: 'Community was successfully destroyed.' }
       format.json { head :no_content }
 		end
+  end
+
+  private
+
+  def set_community
+    @community = Community.find(params[:id])
   end
 
   def catch_not_found(e)
