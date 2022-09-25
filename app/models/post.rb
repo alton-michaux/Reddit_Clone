6551da3ae1ca9fd7221a09846f268cc469187d37
Alton Michaux <alton.michaux@yahoo.com>
2022-09-25 14:46:58 -0400

    moved community options to its own partial

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,11 +6,4 @@ class Post < ApplicationRecord
 
   validates_presence_of :title, :body, :account_id, :community_id, :upvotes, :downvotes
   validates_associated :account, :community
-  # account_community
-
-  # def account_community
-  #   if self.account.communities.include?(self.community)
-  #     errors.add(:account, "Must be a member to post")
-  #   end
-  # end
 end
