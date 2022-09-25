837ffa3e7ade8567bc90ec5e436033e0a6e4c690
Alton Michaux <alton.michaux@yahoo.com>
2022-09-25 12:59:35 -0400

    removing some unnecessary code

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,6 @@ Rails.application.routes.draw do
     resources :subscriptions
   end
 
-  # resources :subscriptions
-  # post 'subscriptions/' => 'subscriptions#create', as: :new_subscription
-  # get 'account/:id/subscriptions' => 'account_subscriptions#index', as: 'account_subscriptions'
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'public#index'
 end
