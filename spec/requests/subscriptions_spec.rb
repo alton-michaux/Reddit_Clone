f14196c3811ffbeca1375d84f63e38c5bbe3a955
Alton Michaux <alton.michaux@yahoo.com>
2022-09-25 12:17:07 -0400

    tests created and passing for subscriptions

--- spec/requests/subscriptions_spec.rb
+++ spec/requests/subscriptions_spec.rb
@@ -7,18 +7,16 @@ RSpec.describe "Subscriptions", type: :request do
   describe "posts new subscription path" do
     it "creates a new subscription" do
       sign_in account
-      subscription_params = FactoryBot.attributes_for(:subscription, account_id: account.id, community_id: community.id)
-      byebug
-      expect { post subscriptions_path, params: { subscription: subscription_params } }.to change(Subscription, :count).by(1)
+      expect { post community_subscriptions_path(account_id: account.id, community_id: community.id) }.to change(Subscription, :count).by(1)
       expect(response).to redirect_to community_path(id: community.id)
-      sign_out subject
+      sign_out account
     end
   end
   describe "delete subscription path" do
     it "deletes a subscription" do
       sign_in account
-      subscription = FactoryBot.create(:subscription, account_id: account.id)
-      expect { delete subscription_path(id: subscription.id) }.to change(Subscription, :count).by(-1)
+      subscription = FactoryBot.create(:subscription, account_id: account.id, community_id: community.id)
+      expect { delete community_subscription_path(id: subscription.id, community_id: community.id, account_id: account.id) }.to change(Subscription, :count).by(-1)
       expect(response).to redirect_to communities_path
     end
   end
